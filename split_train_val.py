b1fedba9278c608c44593b9da00f8bc67d51fabc
Francisco Barrios <fran@MacBook-Pro-de-Francisco.local>
2020-12-29 16:22:49 -0300

    mejora de scripts

--- split_train_val.py
+++ split_train_val.py
@@ -2,8 +2,8 @@ import os
 import argparse
 import random
 
-
 parser = argparse.ArgumentParser()
+parser.add_argument("--porcentaje", type=str, default=0.9, help="procentaje entrenamiento y test.")
 parser.add_argument("--directorio_origen", type=str, default="data/custom/images", help="Directorio donde se encuentran todas las imagenes y las etiquetas")
 parser.add_argument("--directorio_destino", type=str, default="data/custom", help="directorio donde se escribira train y test txt")
 opt = parser.parse_args()
@@ -12,13 +12,15 @@ path = opt.directorio_origen
 
 files = os.listdir(path)
 random.shuffle(files)
-train = files[:int(len(files)*0.9)]
-val = files[int(len(files)*0.9):]
+train = files[:int(len(files)*opt.porcentaje)]
+val = files[int(len(files)*opt.porcentaje):]
 
-with open('{}/train2.txt'.format(opt.directorio_destino), 'w') as f:
+
+
+with open('{}/train.txt'.format(opt.directorio_destino), 'w') as f:
     for item in train:
         f.write("{}/{} \n".format(path, item))
 
-with open('{}/valid2.txt'.format(opt.directorio_destino), 'w') as f:
+with open('{}/valid.txt'.format(opt.directorio_destino), 'w') as f:
     for item in val:
         f.write("{}/{} \n".format(path, item))
